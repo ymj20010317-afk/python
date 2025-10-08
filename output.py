606f5171156256b7ce1ac696e2ea18be3db8f520
ymj20010317-afk <ymj20010317@gmail.com>
2025-10-08 11:07:41 +0900

    Add files via upload

---
+++ output.py
@@ -0,0 +1,13 @@
+print("# 하나만 출력합니다.")
+print("Hello Python Programming...!")
+print()
+
+print("# 여러 개를 출력합니다.")
+print(10,20,30,40,50)
+print()
+
+print("# 아무것도 출력하지 않습니다.")
+print("---확인 전용선---")
+print()
+print()
+print("---확인 전용선---")
