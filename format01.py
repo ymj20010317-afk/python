65166fda1cab58c01912605a8537ab75d7d79119
ymj20010317-afk <ymj20010317@gmail.com>
2025-10-11 03:16:04 +0900

    Add files via upload

---
+++ format01.py
@@ -0,0 +1,9 @@
+format_a = "{}만 원".format(5000)
+format_b = "파이썬 열공하여 첫 연봉 {}만 원 만들기".format(5000)
+format_c = "{} {} {}".format(3000, 4000, 5000)
+format_d = "{} {} {}".format(1, "문자열", True)
+
+print(format_a)
+print(format_b)
+print(format_c)
+print(format_d)
